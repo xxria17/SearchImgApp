38e9fed409011ed709186916c0901a5edc074877
xxria17 <xxrcy32@gmail.com>
2021-11-11 15:49:51 +0900

    불필요한 MainFragment 제거후 MainActivity 로 수정

--- SearchImgApp/app/src/main/java/com/dohyun/searchimgapp/view/MainActivity.kt
+++ SearchImgApp/app/src/main/java/com/dohyun/searchimgapp/view/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.dohyun.searchimgapp.view
 
 import android.os.Bundle
-import androidx.fragment.app.commitNow
 import com.dohyun.searchimgapp.R
 import com.dohyun.searchimgapp.databinding.ActivityMainBinding
 import com.dohyun.searchimgapp.view.base.BaseActivity
@@ -12,10 +11,5 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        supportFragmentManager.commitNow(true) {
-            val mainFragment = MainFragment()
-            replace(R.id.main_content, mainFragment)
-            setPrimaryNavigationFragment(mainFragment)
-        }
     }
 }
