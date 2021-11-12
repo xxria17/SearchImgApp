066aa00325e947d12f341356df8ad97fc22325b5
xxria17 <xxrcy32@gmail.com>
2021-11-12 14:32:13 +0900

    불필요한 메인 액티비티 코드 제거

--- SearchImgApp/app/src/main/java/com/dohyun/searchimgapp/view/MainActivity.kt
+++ SearchImgApp/app/src/main/java/com/dohyun/searchimgapp/view/MainActivity.kt
@@ -1,15 +1,9 @@
 package com.dohyun.searchimgapp.view
 
-import android.os.Bundle
 import com.dohyun.searchimgapp.R
 import com.dohyun.searchimgapp.databinding.ActivityMainBinding
 import com.dohyun.searchimgapp.view.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-}
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main)
